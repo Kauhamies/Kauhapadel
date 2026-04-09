1960622df23b7ccdd514a23525af68d4ee089d1c
Kauhamies <jeremias.koskimaki@outlook.com>
2026-04-09 12:45:57 +0300

    k

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const SUPABASE_ANON_KEY = "sb_publishable_OSNlw36GCL2buO26zgqVNw_N1wZAXQJ";
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 const app = document.getElementById("app");
 
-function renderLogin() {
+function render() {
   app.innerHTML = `
     <div style="padding:40px;font-family:Arial">
       <h1>Kauhapadel</h1>
 
+      <input id="name" placeholder="Nimi" /><br><br>
       <input id="email" placeholder="Sähköposti" /><br><br>
       <input id="password" type="password" placeholder="Salasana" /><br><br>
 
@@ -30,14 +31,23 @@ function renderLogin() {
   };
 
   document.getElementById("signup").onclick = async () => {
+    const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const { error } = await supabase.auth.signUp({ email, password });
+    const { data, error } = await supabase.auth.signUp({ email, password });
+
+    if (!error && data.user) {
+      await supabase.from("profiles").insert({
+        id: data.user.id,
+        name: name,
+        role: "player"
+      });
+    }
 
     document.getElementById("msg").innerText =
       error ? error.message : "Rekisteröinti onnistui";
   };
 }
 
-renderLogin();
+render();
