670a10d63216c80054ceeb047804285ea434546a
Kauhamies <jeremias.koskimaki@outlook.com>
2026-04-09 12:40:26 +0300

    kk

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-document.getElementById("app").innerHTML = `
-  <div style="padding:40px;font-family:Arial,sans-serif">
+const SUPABASE_URL = "https://kgayunrioytcnlirgyvn.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_OSNlw36GCL2buO26zgqVNw_N1wZAXQJ";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+const app = document.getElementById("app");
+
+app.innerHTML = `
+  <div style="padding:40px;font-family:Arial">
     <h1>Kauhapadel</h1>
-    <p>Tämä on uusi testiversio.</p>
+    <p>Supabase yhdistetty</p>
   </div>
 `;
